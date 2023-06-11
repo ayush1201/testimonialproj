54bcc6e660616023a901b7971a04f38e43c9108d
ayush1201 <ayushgupta2429.ag@gmail.com>
2023-06-11 22:36:27 +0530

    testimonial app based on react basics

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Testimonials from "./components/Testimonials";
+import reviews from "./data";
+
+const App = () => {
+  return(
+    <div>
+
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonials reviews={reviews}/>
+      </div>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = ({props}) => {
+    let review = props.review;
+    return(
+        <div>
+            <div>
+                <img src={review.image} alt="" />
+            </div>
+
+            <div>
+                <p>{review.name}</p>
+            </div>
+
+            <div>
+                <p>{review.job}</p> 
+            </div>
+        </div>
+    )
+}
+
+export default Card;
